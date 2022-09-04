71df1dd1df2792485e8b0c2b5c1a44d0fd137df1
Alexander Ford <alxford45@gmail.com>
2022-09-04 19:56:48 +0000

    connected web & api

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web';
+  data = 'loading...';
+  constructor(private http: HttpClient) {
+  }
+  getHello() {
+    return
+  }
+  ngOnInit() {
+    const $hello = this.http.get<{ data: string }>("/api");
+    $hello.subscribe((res) => { this.data = res.data })
+  }
 }
